92d982fff23451fce36c96ffb8e7a241474f55e8
Max Lantas <mnxndev@outlook.com>
2024-02-09 13:39:29 -0800

    render with ReactDOM.createRoot
    ReactDOM.render is not supported anymore: https://reactjs.org/link/switch-to-createroot

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-//import React from 'react';
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
