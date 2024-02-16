e1563a34ce4e7626b1f1c096919e49e2d5765931
Max Lantas <mnxndev@outlook.com>
2024-02-15 16:04:05 -0800

    fix eslint error

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const root = document.getElementById("root");
+if (root !== null) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  console.error("root element is missing");
+}
