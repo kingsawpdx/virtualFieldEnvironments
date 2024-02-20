468b06ca7edcc81d2860aa7b4cec1cf8ea170b35
Sawyer King <kingsawyer555@gmail.com>
2024-02-19 19:50:59 -0800

    Added initial hotspots

--- src/PhotoSphereViewer.tsx
+++ src/PhotoSphereViewer.tsx
@@ -1,11 +1,52 @@
-import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
+import { ViewerConfig } from "@photo-sphere-viewer/core";
+import {
+  MarkersPlugin,
+  ReactPhotoSphereViewer,
+} from "react-photo-sphere-viewer";
 
 import sampleScene from "./assets/VFEdata/ERI_Scene6-IMG_20231006_081813_00_122.jpg";
 
 function PhotoSphereViewer() {
+  const baseUrl = "https://photo-sphere-viewer-data.netlify.app/assets/";
+
+  const plugins: ViewerConfig["plugins"] = [
+    [
+      MarkersPlugin,
+      {
+        markers: [
+          {
+            id: "image",
+            image: baseUrl + "pictos/pin-blue.png",
+            size: { width: 64, height: 64 },
+            position: { yaw: "-5deg", pitch: "5deg" },
+            tooltip: "Outcrop wideview",
+          },
+          {
+            id: "image2",
+            image: baseUrl + "pictos/pin-blue.png",
+            size: { width: 64, height: 64 },
+            position: { yaw: "18deg", pitch: "-3deg" },
+            content: "Look at these flowers",
+            tooltip: "Flowers",
+          },
+          {
+            id: "image3",
+            image: baseUrl + "pictos/pin-blue.png",
+            size: { width: 64, height: 64 },
+            position: { yaw: "45deg", pitch: "10deg" },
+            tooltip: "Modify!",
+          },
+        ],
+      },
+    ],
+  ];
+
+  console.log({ plugins });
+
   return (
     <ReactPhotoSphereViewer
       src={sampleScene}
+      plugins={plugins}
       height={"100vh"}
       width={"100%"}
     ></ReactPhotoSphereViewer>
