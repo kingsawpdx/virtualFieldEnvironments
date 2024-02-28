afdf95043b327a668d9c34fd46aabbc493253127
ashtonesawyer <ashtonesawyer@gmail.com>
2024-02-28 12:45:07 -0800

    convertHotspots fxn - need icons and display for non-image/video content

--- src/PhotosphereViewer.tsx
+++ src/PhotosphereViewer.tsx
@@ -10,23 +10,9 @@ import {
 } from "react-photo-sphere-viewer";
 
 import { Hotspot3D, NavMap, Photosphere } from "./DataStructures";
-import Contact from "./assets/VFEdata/Contact.png";
 import sampleScene from "./assets/VFEdata/ERI_Scene6-IMG_20231006_081813_00_122.jpg";
 import audioFile from "./assets/VFEdata/Scene12_UnevenStandTop_LS100146.mp3";
-import SouthwaterFront from "./assets/VFEdata/SouthwaterFront.png";
-import closerLook from "./assets/VFEdata/a-closer-look.jpg";
-import coolLog from "./assets/VFEdata/cool_log.jpeg";
-import flowers from "./assets/VFEdata/flowers.png";
-import handSample from "./assets/VFEdata/hand_sample.png";
-import logNEARshorline from "./assets/VFEdata/logNEARshoreline.png";
 import mapImage from "./assets/VFEdata/map.jpg";
-import mushroom from "./assets/VFEdata/mushroom.png";
-import outcropWide from "./assets/VFEdata/outcropWideView.png";
-import outcropTextures from "./assets/VFEdata/outcrop_textures.mp4";
-import paddlers from "./assets/VFEdata/paddlers.mp4";
-import shorelineSOUTH from "./assets/VFEdata/shorelineSOUTH.mp4";
-import smallPool from "./assets/VFEdata/small_pool.jpg";
-import SEMComp from "./assets/VFEdata/southSEMcomp.png";
 
 function videoContent(src: string): string {
   return `<video controls style="max-width: 100%; max-height: 100%">
@@ -40,133 +26,52 @@ function pictureContent(imageSrc: string) {
     `;
 }
 
-const baseUrl = "https://photo-sphere-viewer-data.netlify.app/assets/";
+/** Convert yaw/pitch degrees from numbers to strings ending in "deg" */
+function degToStr(val: number): string {
+  return String(val) + "deg";
+}
 
+/** Convert hotspots to markers with type-based content/icons */
 function convertHotspots(hotspots: Hotspot3D[]): MarkerConfig[] {
   if (hotspots.length == 0) return [];
 
-  // TODO: conversion from hotspots instead of hardcoding
-  return [
-    {
-      id: "outcropWideView",
-      image: baseUrl + "pictos/pin-blue.png",
+  let markers: MarkerConfig[] = hotspots.map((hotspot) => {
+    let content: string | undefined = undefined;
+    let icon =
+      "https://photo-sphere-viewer-data.netlify.app/assets/pictos/pin-blue.png"; // default
+
+    switch (hotspot.data.tag) {
+      case "Image":
+        content = pictureContent(hotspot.data.src);
+        //icon = imgIcon;
+        break;
+      case "Video":
+        content = videoContent(hotspot.data.src);
+        //icon = videoIcon;
+        break;
+      case "Audio":
+        break;
+      case "Doc":
+        break;
+      case "PhotosphereLink":
+        break;
+      case "URL":
+        break;
+      default:
+        break;
+    }
+
+    return {
+      id: hotspot.tooltip,
+      image: icon,
       size: { width: 64, height: 64 },
-      position: { yaw: "-5deg", pitch: "5deg" },
-      tooltip: "Outcrop wideview",
-      content: pictureContent(outcropWide),
-    },
-    {
-      id: "flowers",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "18deg", pitch: "-3deg" },
-      tooltip: "Flowers",
-      content: pictureContent(flowers),
-    },
-    {
-      id: "shorelineSOUTH",
-      image: baseUrl + "pictos/pin-red.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "172deg", pitch: "-32deg" },
-      tooltip: "shorelineSOUTH.mp4",
-      content: videoContent(shorelineSOUTH),
-    },
-    {
-      id: "paddlers",
-      image: baseUrl + "pictos/pin-red.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-175deg", pitch: "-10deg" },
-      tooltip: "paddlers.mp4",
-      content: videoContent(paddlers),
-    },
-    {
-      id: "small pool",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "100deg", pitch: "-13deg" },
-      content: pictureContent(smallPool),
-      tooltip: "small pool",
-    },
-    {
-      id: "log near shoreline",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-40deg", pitch: "-25deg" },
-      content: pictureContent(logNEARshorline),
-      tooltip: "log near shoreline",
-    },
-    {
-      id: "coolLog",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-33deg", pitch: "-17deg" },
-      content: pictureContent(coolLog),
-      tooltip: "a cool log",
-    },
-    {
-      id: "handSample",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-44deg", pitch: "0deg" },
-      content: pictureContent(handSample),
-      tooltip: "hand sample of stone",
-    },
-    {
-      id: "outcropTextures",
-      image: baseUrl + "pictos/pin-red.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "5deg", pitch: "6deg" },
-      content: videoContent(outcropTextures),
-      tooltip: "outcrop textures video",
-    },
-    {
-      id: "logs",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-37deg", pitch: "-12deg" },
-      tooltip: "How did these huge logs get here?",
-    },
-    {
-      id: "mushroom",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-19deg", pitch: "2deg" },
-      content: pictureContent(mushroom),
-      tooltip: "mushroom",
-    },
-    {
-      id: "closerLook",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-46deg", pitch: "-2deg" },
-      content: pictureContent(closerLook),
-      tooltip: "A closer look",
-    },
-    {
-      id: "SEMComp",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-48deg", pitch: "0deg" },
-      content: pictureContent(SEMComp),
-      tooltip: "South SEM Comparison",
-    },
-    {
-      id: "Contact",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-46deg", pitch: "2deg" },
-      content: pictureContent(Contact),
-      tooltip: "Contact",
-    },
-    {
-      id: "South Waterfront",
-      image: baseUrl + "pictos/pin-blue.png",
-      size: { width: 64, height: 64 },
-      position: { yaw: "-25deg", pitch: "-1deg" },
-      content: pictureContent(SouthwaterFront),
-      tooltip: "South Waterfront",
-    },
-  ];
+      position: { yaw: degToStr(hotspot.yaw), pitch: degToStr(hotspot.pitch) },
+      tooltip: hotspot.tooltip,
+      content: content,
+    };
+  });
+
+  return markers;
 }
 
 function convertMap(map: NavMap): MapPluginConfig {
@@ -219,6 +124,7 @@ function convertMap(map: NavMap): MapPluginConfig {
     ],
   };
 }
+
 export interface PhotosphereViewerProps {
   photosphere: Photosphere;
   map: NavMap;
