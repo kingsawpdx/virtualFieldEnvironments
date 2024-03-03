30bbfd276ceee8f2cc88d1d1dd331a399a05613b
JohnAsencio <jasencio7182@gmail.com>
2024-03-02 22:34:17 -0800

    added button to top right corner for audio

--- src/PhotosphereViewer.tsx
+++ src/PhotosphereViewer.tsx
@@ -227,6 +227,7 @@ export interface PhotosphereViewerProps {
 function PhotosphereViewer(props: PhotosphereViewerProps) {
   const [isUserInteracted, setIsUserInteracted] = useState(false);
   const photoSphereRef = React.createRef<ViewerAPI>();
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
 
   // handle change of panoramic image
   useEffect(() => {
@@ -255,7 +256,7 @@ function PhotosphereViewer(props: PhotosphereViewerProps) {
   useEffect(() => {
     const audio = new Audio(audioFile);
     // Check if theres user interaction
-    if (isUserInteracted) {
+    if (isUserInteracted && isAudioPlaying) {
       //Make a new audio object with the imported audio file
       //Try to play the audio file, have to use void to indicate were not going to promise to handle the returned type
       void audio.play().catch((e) => {
@@ -270,11 +271,17 @@ function PhotosphereViewer(props: PhotosphereViewerProps) {
       }
     };
     //Depends on the isUserInteracted state, reruns if it changes
-  }, [isUserInteracted]);
+  }, [isUserInteracted, isAudioPlaying]);
 
   //Handler function to set the state to true
   function handleUserInteraction() {
     setIsUserInteracted(true);
+    setIsAudioPlaying(!isAudioPlaying);
+  }
+
+  //toggles state of audio upon button press
+  function toggleAudio() {
+    setIsAudioPlaying((prevIsAudioPlaying) => !prevIsAudioPlaying);
   }
 
   // If I cant get the user interaction forced I made the button
@@ -308,13 +315,31 @@ function PhotosphereViewer(props: PhotosphereViewerProps) {
   console.log(props.map);
 
   return (
+    //if user already interacted start, then display audio button
+    <div>
+    {isUserInteracted && (
+      <button onClick={toggleAudio}
+      style= {{
+          position: 'absolute',
+          zIndex: 1000,
+          top: '18px', // Adjust this value to change the vertical position
+          left: '1325px', // Adjust this value to change the horizontal position
+      }}>
+        {isAudioPlaying ? 'Pause Audio' : 'Play Audio'}
+      </button>
+    )}
+    
+
     <ReactPhotoSphereViewer
       ref={photoSphereRef}
       src={sampleScene}
       plugins={plugins}
       height={"100vh"}
       width={"100%"}
+
+      
     />
+    </div>
   );
 }
 
