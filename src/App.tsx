0b6f1ea0047a586fb8e42d954d9e984d773102a8
khasson <khasson@pdx.edu>
2024-03-27 14:48:31 -0700

    changed some logic in App.tsx to handle the states for swapping views

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import LandingPage from "./LandingPage.tsx";
-import App from "./Prototype.tsx";
 import CreateVFEForm from "./CreateVFE.tsx";
-import PhotosphereViewer from "./PhotosphereViewer.tsx";
 import { VFE } from "./DataStructures.ts";
+import LandingPage from "./LandingPage.tsx";
+import PhotosphereViewer from "./PhotosphereViewer.tsx";
+import App from "./Prototype.tsx";
 
 // Main component acts as a main entry point for the application
 // Should decide what we are doing, going to LandingPage/Rendering VFE
@@ -12,38 +12,38 @@ const AppRoot = () => {
   // Decide state, should manage whether the VFE should be displayed or the LandingPage should be displayed
   const [showApp, setShowApp] = useState(false);
   const [showCreateVFEForm, setShowCreateVFEForm] = useState(false);
-  const [vfeData, setVFEData] = useState<VFE | null>(null)
+  const [vfeData, setVFEData] = useState<VFE | null>(null);
 
   //Create a function to set useState true
   const handleLoadTestVFE = () => {
     setShowApp(true);
+    setShowCreateVFEForm(false);
   };
 
   const handleCreateVFE = () => {
-    setShowCreateVFEForm(true)
-  }
+    setShowCreateVFEForm(true);
+    setShowApp(false);
+  };
 
-  const loadCreatedVFE = (data:VFE) => {
-    setVFEData(data)
-    setShowApp(true)
-  }
+  const loadCreatedVFE = (data: VFE) => {
+    setVFEData(data);
+    setShowApp(true);
+    setShowCreateVFEForm(false);
+  };
 
   return (
     <div>
-    {!showApp && !showCreateVFEForm ? (
-      <LandingPage
-        onLoadTestVFE={handleLoadTestVFE}
-        onCreateVFE={handleCreateVFE}
-      />
-    ) : showCreateVFEForm ? (
-      <CreateVFEForm onCreateVFE={loadCreatedVFE}  />
-    ) : (
-      <>
-      <App />
-      {vfeData && <PhotosphereViewer vfe={vfeData} />}
-      </>
-    )}
-  </div>
+      {showCreateVFEForm ? (
+        <CreateVFEForm onCreateVFE={loadCreatedVFE} />
+      ) : vfeData && showApp ? (
+        <PhotosphereViewer vfe={vfeData} />
+      ) : (
+        <LandingPage
+          onLoadTestVFE={handleLoadTestVFE}
+          onCreateVFE={handleCreateVFE}
+        />
+      )}
+    </div>
   );
 };
 
