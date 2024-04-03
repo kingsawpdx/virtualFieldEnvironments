6b11ba1291d6fd68d001b499e28e2946e133d0e8
khasson <khasson@pdx.edu>
2024-04-03 15:07:28 -0700

    Added basic buttons to the overlay

--- src/AddPhotosphere.tsx
+++ src/AddPhotosphere.tsx
@@ -1,4 +1,17 @@
-function AddPhotosphere() {
+import { useState } from "react";
+
+import { Photosphere, VFE } from "./DataStructures";
+
+interface AddPhotosphereProps {
+  vfe: VFE;
+}
+
+function AddPhotosphere({ vfe }: AddPhotosphereProps): JSX.Element {
+  console.log("Accessing VFE data in AddPhotosphere:", vfe);
+  const [photosphereID, setPhosphereID] = useState("");
+  const [panoImage, setPanoImage] = useState("");
+  const [audioFile, setAudiofile] = useState("");
+
   // function to
   return (
     <div
@@ -13,7 +26,21 @@ function AddPhotosphere() {
         padding: "10px",
       }}
     >
-      <h1>New Photo Sphere</h1>
+      <h1>New Photosphere</h1>
+      <div>
+        <label htmlFor="photosphereID">Photosphere Name:</label>
+        <input type="text" id="photosphereID" />
+      </div>
+      <div>
+        <label htmlFor="panoImage">Panoramic Image:</label>
+        <input type="file" id="panoImage" accept="image/*" />
+      </div>
+      <div>
+        <label htmlFor="audioFile">Background Audio:</label>
+        <input type="file" id="audioFile" accept="audio/*" />
+      </div>
+      {/* This should call a function to add the photosphere to the VFE */}
+      <button>Add Photosphere</button>
     </div>
   );
   // popover
