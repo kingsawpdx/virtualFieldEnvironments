3e0e7d79972abd9cfc895e4afb1a2c8cc95aa3ec
kingsawpdx <kingsaw@pdx.edu>
2024-04-18 16:40:04 -0700

    Added localstorage but buggy

--- src/Prototype.tsx
+++ src/Prototype.tsx
@@ -7,16 +7,17 @@ import {
   VFE,
 } from "./DataStructures.ts";
 import PhotosphereViewer from "./PhotosphereViewer.tsx";
-import EastScene from "./assets/VFEdata/ERI_East-Scene9-IMG_20231006_092207_00_125.jpg";
-import EntranceScene from "./assets/VFEdata/ERI_Entrance-Scene0-IMG_20231006_080232_00_120.jpg";
-import NorthScene from "./assets/VFEdata/ERI_North-Scene8-IMG_20231006_091050_00_124.jpg";
-import SouthScene from "./assets/VFEdata/ERI_SouthScene_6-IMG_20231006_081813_00_122.jpg";
-import WestScene from "./assets/VFEdata/ERI_West-Scene7-IMG_20231006_084114_00_123.jpg";
-import audioFile from "./assets/VFEdata/Scene12_UnevenStandTop_LS100146.mp3";
-import mapImage from "./assets/VFEdata/map.jpg";
 import dataArray from "./data.json";
 
 function App() {
+  //It works with the imported data
+
+  window.localStorage.setItem("vfeData", JSON.stringify(dataArray));
+
+  //It does not work with VFEData as it has type any
+  const VFEData = JSON.parse(window.localStorage.getItem("vfeData") || "{}");
+
+  //If nested hotspots, the nested data is returned as a record.
   function hotspotArrayExtract(
     hotspotArray: Record<string, Hotspot2D>,
   ): Record<string, Hotspot2D> {
@@ -39,6 +40,7 @@ function App() {
     return returnObject;
   }
 
+  //Evaluates hotspot type and returns relevant fields.
   function hotspotDataEvaluation(hotspot: HotspotData): HotspotData {
     switch (hotspot.tag) {
       case "Image":
@@ -78,137 +80,12 @@ function App() {
         break;
     }
   }
-
-  const southHotspotArray: Hotspot3D[] = Object.values(
-    dataArray.vfeData.photospheres.south.hotspots,
-  ).map((hotspot) => {
-    return {
-      pitch: Number(hotspot.pitch),
-      yaw: Number(hotspot.yaw),
-      tooltip: hotspot.tooltip,
-      data: hotspotDataEvaluation(hotspot.data),
-    };
-  });
-
-  const south: Photosphere = {
-    id: "South",
-    src: SouthScene,
-    center: { x: 450, y: 800 },
-    hotspots: Object.fromEntries(
-      southHotspotArray.map((hotspot) => [hotspot.tooltip, hotspot]),
-    ),
-    backgroundAudio: audioFile,
-  };
-
-  const west: Photosphere = {
-    id: "West",
-    src: WestScene,
-    center: { x: 95, y: 530 },
-    hotspots: {
-      "Go North": {
-        pitch: 0,
-        yaw: 10,
-        tooltip: "Go North",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "North",
-        },
-      },
-      "Go South": {
-        pitch: -3,
-        yaw: 115,
-        tooltip: "Go South",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "South",
-        },
-      },
-    },
-  };
-
-  const north: Photosphere = {
-    id: "North",
-    src: NorthScene,
-    center: { x: 390, y: 50 },
-    hotspots: {
-      "Go West": {
-        pitch: 0,
-        yaw: -130,
-        tooltip: "Go West",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "West",
-        },
-      },
-      "Go East": {
-        pitch: 0,
-        yaw: 92,
-        tooltip: "Go East",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "East",
-        },
-      },
-    },
-  };
-
-  const east: Photosphere = {
-    id: "East",
-    src: EastScene,
-    center: { x: 550, y: 450 },
-    hotspots: {
-      "Go North": {
-        pitch: 3,
-        yaw: 2,
-        tooltip: "Go North",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "North",
-        },
-      },
-      "Go to Entrance": {
-        pitch: -2,
-        yaw: -167,
-        tooltip: "Go to Entrance",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "Entrance",
-        },
-      },
-    },
-  };
-
-  const entrance: Photosphere = {
-    id: "Entrance",
-    src: EntranceScene,
-    center: { x: 650, y: 930 },
-    hotspots: {
-      "Go East": {
-        pitch: -2,
-        yaw: -4,
-        tooltip: "Go East",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "East",
-        },
-      },
-      "Go South": {
-        pitch: -4,
-        yaw: -78,
-        tooltip: "Go South",
-        data: {
-          tag: "PhotosphereLink",
-          photosphereID: "South",
-        },
-      },
-    },
-  };
-
+  console.log(typeof VFEData.vfeData.photospheres);
+  console.log(typeof dataArray.vfeData.photospheres);
+  //Create a record of photospheres for each photosphere imported.
   const photospheres: Record<string, Photosphere> = Object.values(
     dataArray.vfeData.photospheres,
   ).reduce((key, photosphere) => {
-    // let photosphereEntry: Record<string, Photosphere> = {}
-
     //Create a Hotspot3D[] for each photosphere
     const photosphereHotspotArray: Hotspot3D[] = Object.values(
       photosphere.hotspots,
@@ -225,10 +102,9 @@ function App() {
       id: photosphere.id,
       src: photosphere.src,
       center: {
-        x: photosphere.center.x,
-        y: photosphere.center.y,
+        x: Number(photosphere.center.x),
+        y: Number(photosphere.center.y),
       },
-
       hotspots: Object.fromEntries(
         photosphereHotspotArray.map((hotspot) => [hotspot.tooltip, hotspot]),
       ),
@@ -236,23 +112,10 @@ function App() {
     return key;
   }, {});
 
-  console.log("New photospheres");
-  console.log(photospheres);
-
-  const oldPhotospheres: Record<string, Photosphere> = {
-    [entrance.id]: entrance,
-    [south.id]: south,
-    [west.id]: west,
-    [north.id]: north,
-    [east.id]: east,
-  };
-  console.log("Old photospheres");
-  console.log(oldPhotospheres);
-
   const map: NavMap = {
-    src: mapImage,
-    rotation: 0,
-    defaultZoom: 20,
+    src: dataArray.vfeData.map.src,
+    rotation: Number(dataArray.vfeData.map.rotation),
+    defaultZoom: Number(dataArray.vfeData.map.defaultZoom),
     hotspots: Object.values(photospheres).map((p) => {
       return {
         x: p.center.x,
@@ -266,9 +129,9 @@ function App() {
   };
 
   const data: VFE = {
-    name: "prototypeElkIslandVFE",
+    name: dataArray.name,
     map: map,
-    defaultPhotosphereID: south.id,
+    defaultPhotosphereID: dataArray.defaultID,
     photospheres,
   };
 
