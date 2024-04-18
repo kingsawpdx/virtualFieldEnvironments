985570b58eacaf440b5760218de79df090c28da8
kingsawpdx <kingsaw@pdx.edu>
2024-04-18 10:48:24 -0700

    Support importing data through json file

--- src/Prototype.tsx
+++ src/Prototype.tsx
@@ -1,210 +1,101 @@
-import { Hotspot3D, NavMap, Photosphere, VFE } from "./DataStructures.ts";
+import {
+  Hotspot2D,
+  Hotspot3D,
+  HotspotData,
+  NavMap,
+  Photosphere,
+  VFE,
+} from "./DataStructures.ts";
 import PhotosphereViewer from "./PhotosphereViewer.tsx";
-import Contact from "./assets/VFEdata/Contact.png";
 import EastScene from "./assets/VFEdata/ERI_East-Scene9-IMG_20231006_092207_00_125.jpg";
 import EntranceScene from "./assets/VFEdata/ERI_Entrance-Scene0-IMG_20231006_080232_00_120.jpg";
 import NorthScene from "./assets/VFEdata/ERI_North-Scene8-IMG_20231006_091050_00_124.jpg";
 import SouthScene from "./assets/VFEdata/ERI_SouthScene_6-IMG_20231006_081813_00_122.jpg";
 import WestScene from "./assets/VFEdata/ERI_West-Scene7-IMG_20231006_084114_00_123.jpg";
 import audioFile from "./assets/VFEdata/Scene12_UnevenStandTop_LS100146.mp3";
-import SouthwaterFront from "./assets/VFEdata/SouthwaterFront.png";
-import CloserLook from "./assets/VFEdata/a-closer-look.jpg";
-import CoolLog from "./assets/VFEdata/cool_log.jpeg";
-import Flowers from "./assets/VFEdata/flowers.png";
-import HandSample from "./assets/VFEdata/hand_sample.png";
-import LogNearShoreline from "./assets/VFEdata/logNEARshoreline.png";
 import mapImage from "./assets/VFEdata/map.jpg";
-import Mushroom from "./assets/VFEdata/mushroom.png";
-import OutcropWide from "./assets/VFEdata/outcropWideView.png";
-import OutcropTextures from "./assets/VFEdata/outcrop_textures.mp4";
-import Paddlers from "./assets/VFEdata/paddlers.mp4";
-import ShorelineSOUTH from "./assets/VFEdata/shorelineSOUTH.mp4";
-import SmallPool from "./assets/VFEdata/small_pool.jpg";
-import SEMComp from "./assets/VFEdata/southSEMcomp.png";
+import dataArray from "./data.json";
 
 function App() {
-  const hotspotArray: Hotspot3D[] = [
-    {
-      pitch: 5,
-      yaw: -5,
-      tooltip: "OutcropWideView",
-      data: {
-        tag: "Image",
-        src: OutcropWide,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: -3,
-      yaw: 18,
-      tooltip: "Flowers",
-      data: {
-        tag: "Image",
-        src: Flowers,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: -32,
-      yaw: 172,
-      tooltip: "ShorelineSOUTH.mp4",
-      data: {
-        tag: "Video",
-        src: ShorelineSOUTH,
-      },
-    },
-    {
-      pitch: -10,
-      yaw: -175,
-      tooltip: "Paddlers.mp4",
-      data: {
-        tag: "Video",
-        src: Paddlers,
-      },
-    },
-    {
-      pitch: -13,
-      yaw: 100,
-      tooltip: "SmallPool",
-      data: {
-        tag: "Image",
-        src: SmallPool,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: -25,
-      yaw: -40,
-      tooltip: "LogNearShoreline",
-      data: {
-        tag: "Image",
-        src: LogNearShoreline,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: -17,
-      yaw: -33,
-      tooltip: "CoolLog",
-      data: {
-        tag: "Image",
-        src: CoolLog,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: 6,
-      yaw: 5,
-      tooltip: "OutcropTextures",
-      data: {
-        tag: "Video",
-        src: OutcropTextures,
-      },
-    },
-    {
-      pitch: -12,
-      yaw: -37,
-      tooltip: "How did these huge logs get here?",
-      data: {
-        tag: "Doc",
-        content: "How did these huge logs get here?",
-      },
-    },
-    {
-      pitch: 2,
-      yaw: -19,
-      tooltip: "Mushroom",
-      data: {
-        tag: "Image",
-        src: Mushroom,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: 2,
-      yaw: -46,
-      tooltip: "Contact",
-      data: {
-        tag: "Image",
-        src: Contact,
-        hotspots: {
-          HandSample: {
-            x: 10,
-            y: 27.5,
-            id: "HandSample",
-            tooltip: "HandSample",
-            color: "#FF0000",
-            data: {
-              tag: "Image",
-              src: HandSample,
-              hotspots: {
-                CloserLook: {
-                  x: 50,
-                  y: 50,
-                  id: "CloserLook",
-                  tooltip: "CloserLook",
-                  color: "#00FF00",
-                  data: {
-                    tag: "Image",
-                    src: CloserLook,
-                    hotspots: {
-                      SouthSEMComparison: {
-                        x: 52.5,
-                        y: 37.5,
-                        id: "SouthSEMComparison",
-                        tooltip: "SouthSEMComparison",
-                        color: "#00FFFF",
-                        data: {
-                          tag: "Image",
-                          src: SEMComp,
-                          hotspots: {},
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-    {
-      pitch: -1,
-      yaw: -25,
-      tooltip: "SouthWaterfront",
-      data: {
-        tag: "Image",
-        src: SouthwaterFront,
-        hotspots: {},
-      },
-    },
-    {
-      pitch: 0,
-      yaw: -35,
-      tooltip: "Go West",
-      data: {
-        tag: "PhotosphereLink",
-        photosphereID: "West",
-      },
-    },
-    {
-      pitch: -7,
-      yaw: 125,
-      tooltip: "Go to Entrance",
-      data: {
-        tag: "PhotosphereLink",
-        photosphereID: "Entrance",
-      },
-    },
-  ];
+  function hotspotArrayExtract(
+    hotspotArray: Record<string, Hotspot2D>,
+  ): Record<string, Hotspot2D> {
+    const keys = Object.keys(hotspotArray);
+    const firstKey = keys[0];
+
+    const returnString = hotspotArray[firstKey].tooltip;
+
+    const returnObject: Record<string, Hotspot2D> = {};
+
+    returnObject[returnString] = {
+      x: Number(hotspotArray[firstKey].x),
+      y: Number(hotspotArray[firstKey].y),
+      id: hotspotArray[firstKey].id,
+      tooltip: hotspotArray[firstKey].tooltip,
+      color: hotspotArray[firstKey].color,
+      data: hotspotDataEvaluation(hotspotArray[firstKey].data),
+    };
+
+    return returnObject;
+  }
+
+  function hotspotDataEvaluation(hotspot: HotspotData): HotspotData {
+    switch (hotspot.tag) {
+      case "Image":
+        let hotspotArray: Record<string, Hotspot2D> = {};
+
+        if (hotspot.hotspots) {
+          hotspotArray = hotspotArrayExtract(hotspot.hotspots);
+        } else {
+          hotspotArray = {};
+        }
+
+        return {
+          tag: "Image",
+          src: hotspot.src,
+          hotspots: hotspotArray,
+        };
+        break;
+      case "Video":
+      case "URL":
+      case "Audio":
+        return {
+          tag: hotspot.tag,
+          src: hotspot.src,
+        };
+        break;
+      case "Doc":
+        return {
+          tag: hotspot.tag,
+          content: "",
+        };
+        break;
+      case "PhotosphereLink":
+        return {
+          tag: hotspot.tag,
+          photosphereID: hotspot.photosphereID,
+        };
+        break;
+    }
+  }
+
+  const southHotspotArray: Hotspot3D[] = Object.values(
+    dataArray.vfeData.photospheres.south.hotspots,
+  ).map((hotspot) => {
+    return {
+      pitch: Number(hotspot.pitch),
+      yaw: Number(hotspot.yaw),
+      tooltip: hotspot.tooltip,
+      data: hotspotDataEvaluation(hotspot.data),
+    };
+  });
 
   const south: Photosphere = {
     id: "South",
     src: SouthScene,
     center: { x: 450, y: 800 },
     hotspots: Object.fromEntries(
-      hotspotArray.map((hotspot) => [hotspot.tooltip, hotspot]),
+      southHotspotArray.map((hotspot) => [hotspot.tooltip, hotspot]),
     ),
     backgroundAudio: audioFile,
   };
@@ -313,13 +204,50 @@ function App() {
     },
   };
 
-  const photospheres: Record<string, Photosphere> = {
+  const photospheres: Record<string, Photosphere> = Object.values(
+    dataArray.vfeData.photospheres,
+  ).reduce((key, photosphere) => {
+    // let photosphereEntry: Record<string, Photosphere> = {}
+
+    //Create a Hotspot3D[] for each photosphere
+    const photosphereHotspotArray: Hotspot3D[] = Object.values(
+      photosphere.hotspots,
+    ).map((hotspot) => {
+      return {
+        pitch: Number(hotspot.pitch),
+        yaw: Number(hotspot.yaw),
+        tooltip: hotspot.tooltip,
+        data: hotspotDataEvaluation(hotspot.data),
+      };
+    });
+
+    key[photosphere.id] = {
+      id: photosphere.id,
+      src: photosphere.src,
+      center: {
+        x: photosphere.center.x,
+        y: photosphere.center.y,
+      },
+
+      hotspots: Object.fromEntries(
+        photosphereHotspotArray.map((hotspot) => [hotspot.tooltip, hotspot]),
+      ),
+    };
+    return key;
+  }, {});
+
+  console.log("New photospheres");
+  console.log(photospheres);
+
+  const oldPhotospheres: Record<string, Photosphere> = {
     [entrance.id]: entrance,
     [south.id]: south,
     [west.id]: west,
     [north.id]: north,
     [east.id]: east,
   };
+  console.log("Old photospheres");
+  console.log(oldPhotospheres);
 
   const map: NavMap = {
     src: mapImage,
