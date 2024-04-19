54af96a910035278891b32a8e22cac9f21946e69
Genevieve Latimer <genevievelatimer@gmail.com>
2024-04-19 14:04:43 -0700

    Updated the Header and sidebar to fix overlapping. I need to add responsiveness using mui.

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -25,86 +25,86 @@ function LandingPage({ onLoadTestVFE, onCreateVFE }: LandingPageProps) {
     // MUI template for clipped drawer landing page layout
     // https://mui.com/material-ui/react-drawer/
     <Box sx={{ display: "flex" }}>
-      <CssBaseline>
-        <AppBar
-          position="fixed"
-          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        >
-          <Toolbar>
-            <Typography variant="h6" noWrap component={"div"}>
-              Virtual Field Guide Editor
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          sx={{
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h2" noWrap component={"div"}>
+            Virtual Field Guide Editor
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
             width: drawerWidth,
-            flexShrink: 0,
-            [`& .MuiDrawer-paper`]: {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
-          }}
-        ></Drawer>
-        <Box sx={{ overflow: "auto" }}>
-          <List>
-            {[
-              "View Prototype",
-              "Add Virtual Field Environment",
-              "Add Prototype",
-            ].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? (
-                      <DesktopWindowsSharpIcon onClick={onLoadTestVFE} />
-                    ) : (
-                      <LibraryAddSharpIcon onClick={onCreateVFE} />
-                    )}
-                  </ListItemIcon>
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </CssBaseline>
+            boxSizing: "border-box",
+            marginTop: "70px",
+          },
+        }}
+      >
+        <Toolbar>
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              {[
+                "View Prototype",
+                "Add Virtual Field Environment",
+                "Add Prototype",
+              ].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? (
+                        <DesktopWindowsSharpIcon onClick={onLoadTestVFE} />
+                      ) : (
+                        <LibraryAddSharpIcon onClick={onCreateVFE} />
+                      )}
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Toolbar>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "64px" }}>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box>
     </Box>
   );
 }
 
 export default LandingPage;
-// <div
-//   style={{
-//     height: "100vh",
-//     display: "flex",
-//     flexDirection: "column",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     background: "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)",
-//   }}
-// >
-//   <h1> Welcome to the Virtual Field Enviornment (VFE) </h1>
-//   <p> Explore and create virtual field enviornments! </p>
-
-//   <Button
-//     variant="contained"
-//     onClick={onLoadTestVFE}
-//     style={{
-//       padding: "10px 20px",
-//       margin: "20px",
-//     }}
-//   >
-//     Open Prototype
-//   </Button>
-//   <Button
-//     variant="contained"
-//     onClick={onCreateVFE}
-//     style={{
-//       padding: "10px 20px",
-//       margin: "20px",
-//     }}
-//   >
-//     Create Virtual Field Environment
-//   </Button>
-// </div>
