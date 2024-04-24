759dee01a0964bc1f34c2a71f861a11cb181f373
ashtonesawyer <ashtonesawyer@gmail.com>
2024-04-24 10:49:18 -0700

    rm commented out code

--- src/AddHotspot.tsx
+++ src/AddHotspot.tsx
@@ -242,69 +242,6 @@ function AddHotspot({ onAddHotspot, onCancel, pitch, yaw }: AddHotspotProps) {
         Cancel
       </Button>
     </Box>
-    // <div
-    //   style={{
-    //     position: "absolute",
-    //     zIndex: 1000,
-    //     right: "20px",
-    //     top: "20px",
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     background: "rgba(255, 255, 255, 0.8)",
-    //     borderRadius: "8px",
-    //     padding: "10px",
-    //   }}
-    // >
-    //   <h4 style={{ textAlign: "center" }}>Add A Hotspot</h4>
-    //   <div>
-    //     <label htmlFor="tooptip">Tooltip: </label>
-    //     <input
-    //       type="string"
-    //       id="tooltip"
-    //       value={tooltip}
-    //       onChange={(e) => {
-    //         setTooltip(e.target.value);
-    //       }}
-    //     />
-    //   </div>
-    //   <div>
-    //     <label htmlFor="contentType">Content Type: </label>
-    //     <select
-    //       name="contentType"
-    //       id="contentType"
-    //       onChange={(e) => {
-    //         setContentType(e.target.value);
-    //       }}
-    //     >
-    //       <option value="invalid">-- Select --</option>
-    //       <option value="Image">Image</option>
-    //       <option value="Video">Video</option>
-    //       <option value="Audio">Audio</option>
-    //       <option value="URL">URL</option>
-    //       <option value="Doc">Document</option>
-    //       <option value="PhotosphereLink">Photosphere Link</option>
-    //     </select>
-    //   </div>
-    //   <div>
-    //     <ContentInput contentType={contentType} onChangeContent={setContent} />
-    //   </div>
-    //   <div>
-    //     <span>Pitch: </span>
-    //     <span id="pitch">{String(pitch.toFixed(2))}</span>
-    //     <span>Yaw: </span>
-    //     <span id="yaw">{String(yaw.toFixed(2))}</span>
-    //     <br />
-    //     <span>Click on viewer for pitch and yaw</span>
-    //   </div>
-    //   <div style={{ display: "flex", justifyContent: "space-around" }}>
-    //     <button style={{ width: "40%" }} onClick={handleAddHotspot}>
-    //       Create
-    //     </button>
-    //     <button style={{ width: "40%" }} onClick={onCancel}>
-    //       Cancel
-    //     </button>
-    //   </div>
-    // </div>
   );
 }
 
