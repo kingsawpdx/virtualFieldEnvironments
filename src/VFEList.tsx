04f1352aa33e064d427c039954f3708cc677acc1
khasson <khasson@pdx.edu>
2024-05-05 12:25:19 -0700

    Worked on the VFEList, utilziing the Cards

--- src/VFEList.tsx
+++ src/VFEList.tsx
@@ -1,8 +1,14 @@
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardHeader from "@mui/material/CardHeader";
 import localforage from "localforage";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function VFEList() {
   const [names, setNames] = useState<string[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function load() {
@@ -12,7 +18,29 @@ function VFEList() {
     void load();
   }, []);
 
-  return names.map((name) => <p key={name}>{name}</p>);
+  return names.map((name) => (
+    <Card key={name} sx={{ maxWidth: 345 }}>
+      <CardHeader title={name}> </CardHeader>
+      <CardActions>
+        <Button
+          size="small"
+          onClick={() => {
+            navigate(`/viewer/${name}/`);
+          }}
+        >
+          Viewer
+        </Button>
+        <Button
+          size="small"
+          onClick={() => {
+            navigate(`/editor/${name}/`);
+          }}
+        >
+          Editor
+        </Button>
+      </CardActions>
+    </Card>
+  ));
 }
 
 export default VFEList;
