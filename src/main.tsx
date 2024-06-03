88a74c8014c0781a771fd7cc37eb5a449f59e3b2
Max Lantas <mnxndev@outlook.com>
2024-06-02 18:30:57 -0700

    bundle roboto font

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import { ThemeProvider } from "@emotion/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { createTheme } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
